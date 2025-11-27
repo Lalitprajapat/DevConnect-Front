2ab64441d703cd4fae0cae3e1a36fc2ad1e1f020
Lalit <lalitprajapat9347@gmail.com>
2025-11-27 15:50:21 +0530

    added basic routing

---
+++ src/Login.jsx
@@ -0,0 +1,8 @@
+const Login = () =>{
+    return(
+        <div>
+            <h1 className="text-3xl font-bold">Login Page</h1>
+        </div>
+    );
+};
+export default Login;
---
+++ src/Profile.jsx
@@ -0,0 +1,8 @@
+const Profile = () => {
+    return(
+        <div>
+            <h1 className="text-3xl font-bold">Profile Page</h1>
+        </div>
+    );
+};
+export default Profile;
