04f734899ea498a4d3d8b18f69a0656a190684a5
Lalit <lalitprajapat9347@gmail.com>
2025-11-29 05:30:20 +0530

    solved repeated api calls on refresh and implemented navigation

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { API_BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store)=>store.user);
+    const fetchUser = async () =>{
+        if(userData)return;
+        try{
+            const user = await axios.get(API_BASE_URL+"/profile/view", 
+            {withCredentials:true}
+        );
+        dispatch(addUser(user.data));
+        }catch(err){
+            
+            if(err.status == 401)navigate("/login");
+            console.log("Error fetching user data", err);
+        }
+
+    };
+    useEffect(()=>{
+       fetchUser();
+    },[]);
     return(
         <div>
             <NavBar/>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 
@@ -6,7 +7,7 @@ const NavBar = () => {
     return (
         <div className="navbar bg-base-200 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevConnect</a>
+          <Link to="/" className="btn btn-ghost text-xl">DevConnect</Link>
         </div>
         {user && (<div className="flex gap-2">
           <div className="form-control">Welcome, {user.firstName}</div>
@@ -24,10 +25,10 @@ const NavBar = () => {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
               <li>
-                <a className="justify-between">
+                <Link to='/profile' className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li><a>Settings</a></li>
               <li><a>Logout</a></li>
