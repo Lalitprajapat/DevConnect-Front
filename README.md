dcf324beb87fcdf9097d174f27abe9d5e542ed0f
Lalit <lalitprajapat9347@gmail.com>
2025-11-30 02:41:47 +0530

    featured connection page

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # React + Vite
 
-- Created a React + vite application 
-
-
-
-
-
-
+- Created a React + vite application for DevConnect frontend implementation
+- CORS added CORS in Backend and added middleware with origin configuration
+- added react-redux toolkit to implement react store 
+- added slices for different use cases for configured store
+- added NavBar update as soon as user log in 
+- Refactored code to add add constants file + created a component folder
+- implemented check for token to see if user is logged in or not if not then redirect to login page 
+- added feed in the store 
+- build the user card and implemented real time profile update available to user to see the changes parallel  
+- show toast message on updating profile successfully  
+- connections to see all the connections user have 
 
 
 Body
---
+++ src/components/Connections.jsx
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
+import { API_BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { addConnections } from "../utils/connectionSlice";
+
+const Connections = () => {
+
+    const connections = useSelector((store)=>store.connections);
+    const dispatch = useDispatch();
+    const fetchConnections = async () => {
+        try{
+            const res = await axios.get(API_BASE_URL+"/user/connections",{withCredentials:true});
+            // console.log(res,data.data);
+            dispatch(addConnections(res?.data?.data))
+        }catch(err){
+
+        }
+    };
+    useEffect(()=>{
+        fetchConnections();
+    },[]);
+
+    if(!connections)return;
+    if(connections.length === 0)return <h2>No Connections found</h2>
+    return(
+        <div className="text-center my-10">
+            <h1 className="text-bold text-3xl">Connections</h1>
+            {connections.map((connection)=>{
+                
+                return(
+                    <div className="flex m-4 p-4 rounded-lg bg-base-300 max-w-0.5 mx-auto">
+                        <div>
+                            <img alt="image"  className="w-20 h-20 rounded-full"  src= {connection.photoUrl}/>
+                        </div>
+                        <div className="text-left mx-4 ">    
+                            <h2 className="font-bold text-xl">{connection.firstName + " " + connection.lastName}</h2>
+                            {connection.age && connection.gender && <p>{connection.age + ", " + connection.gender}</p>}
+                            <p>{connection.about}</p>
+                        </div>    
+                    </div>
+                )
+
+            })}
+        </div>
+    );
+};
+export default Connections;
